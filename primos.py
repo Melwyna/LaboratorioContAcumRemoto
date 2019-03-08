4a7a0043efc9360e52ab56cd18347477e288ff76
Lara Moreno <000403982@bga.upb>
2019-03-08 12:38:42 -0500

    Mi primer commit

---
+++ primos.py
@@ -0,0 +1,7 @@
+a=int(input("Ingresa el primer numero"))
+b=int(input("Ingresa el primer numero"))
+if a%b==0:
+ print("La division de los dos numeros es exacta")
+else:
+  if a%b!=0:
+   print("La division de los dos numeros no es exacta")
