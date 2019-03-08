2ae1663d31ab851de911ca23e3db09782a51b93c
Lara Moreno <000403982@bga.upb>
2019-03-08 13:57:02 -0500

    Ejercicio resuelto

--- primos.py
+++ primos.py
@@ -1,5 +1,9 @@
 res=0
 a=int(input("Ingresa el numero:"))
-for i in range (1,a):
- res=a%i
- print(res)
+for i in range (1,a+1,1):
+  if a%i==0:
+    res=res+1
+if res>2:
+ print("no es un numero primo")
+else:
+ print("es primo")
